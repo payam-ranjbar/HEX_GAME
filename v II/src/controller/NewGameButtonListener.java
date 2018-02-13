1bad23362011ccc8c34a8d040f8b8a91c7191d0f
payam <payam.ranjbar@live.com>
2018-02-13 16:50:10 +0330

    turn saver file is rewritten when new game button pressed

--- v II/src/controller/NewGameButtonListener.java
+++ v II/src/controller/NewGameButtonListener.java
@@ -7,11 +7,17 @@ import gui.SettingScreen;
 import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Random;
 
 public class NewGameButtonListener implements MouseListener {
     final static ImageIcon PRESSED = new ImageIcon("assets/newGameClicked.png");
     final static ImageIcon RELEASED = new ImageIcon("assets/newGame.png");
+    final static String TurnSaverFile = "Files/turnsaver.dat";
+
     private SettingScreen settingScreen;
     private JLabel button;
 
@@ -27,6 +33,12 @@ public class NewGameButtonListener implements MouseListener {
     public void mouseClicked(MouseEvent e) {
         settingScreen.setVisible(false);
         GameScreen gameScreen = new GameScreen(new GamePlay(settingScreen.getGameMode(), settingScreen.getBoard()) , settingScreen.getBoard());
+        try {
+            FileOutputStream file = new FileOutputStream(TurnSaverFile);
+            file.write("1".getBytes());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
     }
 
     @Override
