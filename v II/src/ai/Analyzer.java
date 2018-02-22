782d8335809a364760ca30448d6b0c02b5fa4a33
payam <payam.ranjbar@live.com>
2018-02-21 22:49:19 -0800

    AI classes updated

--- v II/src/ai/Analyzer.java
+++ v II/src/ai/Analyzer.java
@@ -132,4 +132,5 @@ public class Analyzer {
         }
         return sameBPoints;
     }
+
 }
--- v II/src/ai/Listener.java
+++ v II/src/ai/Listener.java
@@ -6,4 +6,7 @@ import java.util.ArrayList;
 
 public class Listener {
     public ArrayList<Hexagon> criticalMoves;
+
+
+
 }
