07a9e5acf4eaa8803d61aa8ed0ea515323b26c93
payam <payam.ranjbar@live.com>
2018-02-21 23:43:35 -0800

    AI classes updated

--- v II/src/ai/Performer.java
+++ v II/src/ai/Performer.java
@@ -1,12 +1,22 @@
 package ai;
 
+import entities.Board;
 import entities.GamePlay;
+import entities.States;
+import gui.GameScreen;
 
 public class Performer {
-    GamePlay gamePlay;
 
-    public Performer(){
+    private GamePlay gamePlay;
+    private GameScreen gameScreen;
+
+    public Performer(GamePlay gp , GameScreen gs){
+        gamePlay = gp;
+        gameScreen = gs;
 
     }
-    public void set(){}
+    public void setHex(int x, int y, States state){
+        gameScreen.setHexagonsState(x, y, state);
+        gamePlay.getBoard().hexagons[x][y].setState(state);
+    }
 }
--- v II/src/entities/States.java
+++ v II/src/entities/States.java
@@ -1,9 +1,15 @@
 package entities;
 
+import javax.swing.*;
+
 public enum States {
     blue,
     red,
     empty;
+
+    private final static ImageIcon YELLOW = new ImageIcon("assets/b1.png");
+    private final static ImageIcon BLACK = new ImageIcon("assets/b0.png");
+    private final static ImageIcon RED = new ImageIcon("assets/b2.png");
 
     public States reverse() {
         if (this == red) {
@@ -12,6 +18,15 @@ public enum States {
             return red;
         }
         return empty;
+    }
+    public ImageIcon getImageIcon() {
+        if (this == red) {
+            return RED;
+        } else if (this == blue) {
+            return YELLOW;
+        }
+        return BLACK;
+
     }
     @Override
     public String toString() {
